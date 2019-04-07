4c2ae7dfe1b8dc32c08dbf9548cbc10287a42159
maktag <maktagstudy@gmail.com>
2019-04-07 23:54:13 +0530

    testing..

---
+++ src/test/java/Test/Test/AppTest.java
@@ -0,0 +1,24 @@
+package Test.Test;
+
+import org.testng.annotations.Test;
+
+public class AppTest 
+{
+	@Test
+	public void first(){
+		System.out.println("First");
+	}
+	@Test
+	public void sec(){
+		System.out.println("Second");	
+	}
+	@Test
+	public void third(){
+		System.out.println("Third");
+	}
+	@Test
+	public void fourth(){
+		System.out.println("Fourth");
+	}
+	
+}
---
+++ src/test/java/Test/Test/UnitTest.java
@@ -0,0 +1,27 @@
+package Test.Test;
+
+import org.testng.annotations.Test;
+
+public class UnitTest {
+
+	@Test
+	public void first(){
+		System.out.println("First1");
+	}
+	
+	@Test
+	public void sec(){
+		System.out.println("Second2");	
+	}
+	
+	@Test
+	public void third(){
+		System.out.println("Third3");
+	}
+	
+	@Test
+	public void fourth(){
+		System.out.println("Fourth4");
+	}
+	
+}
